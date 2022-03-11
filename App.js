fe70e82e192354d99e7356d8b8e06524185f8c81
ygrek <ygreksh@gmail.com>
2022-03-11 14:48:23 +0200

    start

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+const App = () => {
+
+  return (
+    <View>
+      <Text>Hello World!</Text>
+    </View>
+  );
+};
+
+
+
+export default App;
