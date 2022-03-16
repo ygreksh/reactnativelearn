2c76175a761e32ca88e9f083f1e93b901b497e25
ygrek <ygreksh@gmail.com>
2022-03-16 11:12:22 +0200

    DONE Loading groups, channels, playing TV stream on press

--- src/Channel.js
+++ src/Channel.js
@@ -1,13 +1,36 @@
 import React from "react";
 import { Text, View, Image, StyleSheet, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import useStore from './store';
+
 
 const Channel = ({channel}) => {
     const navigation = useNavigation();
+    const sid = useStore(state => state.sid);
 
-    const handleOnPress = () => {
+    async function handleOnPress () {
         console.log("Channel : " + channel.name);
-        navigation.navigate("Player", channel);
+
+        const video = {};
+        let videoUrl = null;
+        let baseUrl = 'https://online.polbox.tv/api/json/';
+        let url = baseUrl + "get_url?"+ "cid=" + channel.id;
+        let headers = new Headers();
+        headers.append('Cookie', sid);
+        let result = await fetch(url, {method:'GET',
+                        headers: headers,})
+              .then(response => response.json())
+              .then(json => {
+                    console.log('Channel JSON: ', JSON.stringify(json));
+                    let temp = json.url.replace("http/ts", "http");
+                    let matches = temp.split(' ');
+                    videoUrl = matches[0] + ".m3u8";
+                    console.log('Channel URL: ', videoUrl);
+                    navigation.navigate("Player", {
+                        url: videoUrl
+                    });
+                  });
+
     }
 
     return(
@@ -15,7 +38,7 @@ const Channel = ({channel}) => {
             >
                 <TouchableOpacity onPress={handleOnPress}>
                     <Image
-                        style={{width: 50, height: 50}}
+                        style={{width: 60, height: 60}}
                         source={{uri: 'http://online.polbox.tv/' + channel.icon}}
                     />
                     <Text >
--- src/Player.js
+++ src/Player.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Text, View, StyleSheet } from 'react-native'
 import { NativeRouter, Link } from 'react-router-native'
 import Video from 'react-native-video'
-import useStore from './store';
 
 const styles = StyleSheet.create({
     backgroundVideo: {
@@ -15,36 +14,14 @@ const styles = StyleSheet.create({
   });
 
 const Player = ({route}) => {
-    // const video = {uri: 'https://multiplatform-f.akamaihd.net/i/multi/will/bunny/big_buck_bunny_,640x360_400,640x360_700,640x360_1000,950x540_1500,.f4v.csmil/master.m3u8'};
-    const {id, name} = route.params;
-    const video = {};
-    let videoUrl = null;
-    const sid = useStore(state => state.sid);
-    let baseUrl = 'https://online.polbox.tv/api/json/';
-    let url = baseUrl + "get_url?"+ "cid=" + id;
-    let regexp = "^\S+\s";
-        let headers = new Headers();
-        if (sid) {
-            headers.append('Cookie', sid);
-          fetch(url, {method:'GET',
-                    headers: headers,})
-          .then(response => response.json())
-          .then(json => {
-              console.log('Channel JSON: ', JSON.stringify(json));
-            //   videoUrl = json.url.replace("http/ts", "https").replace("^\S+","");
-                let temp = json.url.replace("http/ts", "http");
-                let matches = temp.split(' ');
-                videoUrl = matches[0] + ".m3u8";
-              console.log('Channel URL: ', videoUrl);
-            //   video = {uri: json.url}
-              });
-          }
-
+    const {url} = route.params;
+    // const {uri} = video;
     return(
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text>Video: {url}</Text>
             <Video
                 style={styles.backgroundVideo}
-                source={{uri: videoUrl}}
+                source={{uri: url}}
                 controls
                 resizeMode="contain"
             />
