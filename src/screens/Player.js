73dfd3d15839681979b164eb32fcc50859532f23
ygrek <ygreksh@gmail.com>
2022-03-29 16:57:29 +0300

    some style for video screen

--- src/screens/Player.js
+++ src/screens/Player.js
@@ -3,6 +3,11 @@ import { Text, View, StyleSheet } from 'react-native'
 import Video from 'react-native-video'
 
 const styles = StyleSheet.create({
+    container: {
+      flex: 1, 
+      alignItems: 'center', 
+      justifyContent: 'center'
+    },
     backgroundVideo: {
       position: 'absolute',
       top: 0,
@@ -10,17 +15,13 @@ const styles = StyleSheet.create({
       bottom: 0,
       right: 0,
     },
-    container: {
-      flex: 1, 
-      alignItems: 'center', 
-      justifyContent: 'center'
-    },
     content: {
       flex: 1,
       justifyContent: 'center',
     },
     text: {
       fontSize: 20,
+      color: 'red',
       textAlign: 'center',
       margin: 10,
     },
@@ -29,18 +30,22 @@ const styles = StyleSheet.create({
 const Player = ({route}) => {
     const {url} = route.params;
     return(
-        <View style={styles.container}>
+        <View 
+          style={styles.container}
+          >
+          <Video
+            style={styles.backgroundVideo}
+            source={{uri: url}}
+            controls={true}
+            resizeMode={"contain"}
+            />
           <Text
             // style={styles.text}
           >
           Video: {url}</Text>
-            <Video
-                style={styles.backgroundVideo}
-                source={{uri: url}}
-                controls={true}
-                resizeMode={"contain"}
-            />
-          <View style={styles.content}>
+          <View 
+            style={styles.content}
+            >
             <Text 
               style ={styles.text} 
             > 
